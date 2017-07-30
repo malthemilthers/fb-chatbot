2cba524154e2432c260da87c8220179aca6c9fd1
Malthe Milthers <malthe@milthers.dk>
2017-07-30 15:17:19 +0200

    bugfix

--- app.js
+++ app.js
@@ -119,70 +119,70 @@ function processMessage(event) {
   }
 }
 
+
+function findMovie(userId, movieTitle) {
+  request("http://www.omdbapi.com/?apikey=bd757374&type=movie&t=" + movieTitle, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+            var movieObj = JSON.parse(body);
+            if (movieObj.Response === "True") {
+                var query = {user_id: userId};
+                var update = {
+                    user_id: userId,
+                    title: movieObj.Title,
+                    plot: movieObj.Plot,
+                    date: movieObj.Released,
+                    runtime: movieObj.Runtime,
+                    director: movieObj.Director,
+                    cast: movieObj.Actors,
+                    rating: movieObj.imdbRating,
+                    poster_url:movieObj.Poster
+                };
+                var options = {upsert: true};
+                Movie.findOneAndUpdate(query, update, options, function(err, mov) {
+                    if (err) {
+                        console.log("Database error: " + err);
+                    } else {
+                        message = {
+                            attachment: {
+                                type: "template",
+                                payload: {
+                                    template_type: "generic",
+                                    elements: [{
+                                        title: movieObj.Title,
+                                        subtitle: "Is this the movie you are looking for?",
+                                        image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
+                                        buttons: [{
+                                            type: "postback",
+                                            title: "Yes",
+                                            payload: "Correct"
+                                        }, {
+                                            type: "postback",
+                                            title: "No",
+                                            payload: "Incorrect"
+                                        }]
+                                    }]
+                                }
+                            }
+                        };
+                        sendMessage(userId, message);
+                    }
+                });
+            } else {
+                console.log(movieObj.Error);
+                sendMessage(userId, {text: movieObj.Error});
+            }
+        } else {
+            sendMessage(userId, {text: "Something went wrong. Try again."});
+        }
+    });
+}
+
 function getMovieDetail(userId, field) {
   Movie.findOne({user_id: userId}, function(err, movie) {
     if(err) {
       sendMessage(userId, {text: "Something went wrong. Try again"});
     } else {
       sendMessage(userId, {text: movie[field]});
-    }
-  });
-}
-
-function findMovie(userId, movieTitle) {
-  console.log('Lets try and find the movie: ' + movieTitle);
-  request("http://www.omdbapi.com/?apikey=bd757374&type=movie&t=" + movieTitle, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var movieObj = JSON.parse(body);
-      if (movieObj.Response === "True") {
-        var query = {user_id: userId};
-        var update = {
-          user_id: userId,
-          title: movieObj.Title,
-          plot: movieObj.Plot,
-          date: movieObj.Released,
-          runtime: movieObj.Runtime,
-          director: movieObj.Director,
-          cast: movieObj.Actors,
-          rating: movieObj.imdbRating,
-          poster_url:movieObj.Poster
-        };
-        var options = {upsert: true};
-        Movie.findOneAndUpdate(query, update, options, function(err, mov) {
-          if (err) {
-            console.log("Database error: " + err);
-          } else {
-            message = {
-              attachment: {
-                type: "template",
-                payload: {
-                  template_type: "generic",
-                  elements: [{
-                    title: movieObj.Title,
-                    subtitle: "Is this the movie you are looking for?",
-                    image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
-                    buttons: [{
-                      type: "postback",
-                      title: "Yes",
-                      payload: "Correct"
-                    }, {
-                      type: "postback",
-                      title: "No",
-                      payload: "Incorrect"
-                    }]
-                  }]
-                }
-              }
-            };
-            sendMessage(userId, message);
-          }
-        });
-      } else {
-          console.log('movieObj.Error is ' + movieObj.Error);
-          sendMessage(userId, {text: movieObj.Error});
-      }
-    } else {
-      sendMessage(userId, {text: "Something went wrong. Try again."});
     }
   });
 }
